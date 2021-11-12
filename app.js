b8d6e93cdf7377bae0579754942e38edce4acbab
Prabakaran2712 <prabakaran271202@gmail.com>
2021-11-12 17:39:30 +0530

    Finished signup page

--- app.js
+++ app.js
@@ -6,13 +6,10 @@ const express               =  require('express'),
       bodyParser            =  require("body-parser"),
       LocalStrategy         =  require("passport-local"),
       passportLocalMongoose =  require("passport-local-mongoose"),
-      User                  =  require("./database");
-
-// Setting Up node_modules
-
-
+      User                  =  require("./database"),
+      flash                 =  require("connect-flash");
+// Setting Up view engine
 app.set("view engine", "ejs");
-
 app.use(express.static("public"));
 //Connecting database
 mongoose.connect("mongodb://localhost/pdaregister");
@@ -22,6 +19,7 @@ app.use(require("express-session")({
     resave: false,          
     saveUninitialized:false    
 }));
+
 passport.serializeUser(User.serializeUser());       //session encoding
 passport.deserializeUser(User.deserializeUser());   //session decoding
 passport.use(new LocalStrategy(User.authenticate()));
@@ -32,12 +30,12 @@ app.use(bodyParser.urlencoded(
 app.use(passport.initialize());
 app.use(passport.session())
 
-// Main Directory
+// Routes
 app.get("/", function(req, res) {
     res.render("home");
 });
 app.get("/login", function(req,res) {
-  res.sendFile(__dirname + "/views/login.html");
+  res.render("login");
 });
 app.post("/", function(req, res) {
     res.redirect("/");
@@ -57,55 +55,16 @@ app.post("/register", function(req, res) {
   var cpassword=req.body['cpass'];
   var email=req.body['email'];
   console.log(name1);
-  // User.find({username:rollnumber}).count(function(err,val){
-  //   if(err) console.log(err);
-  //   console.log(val);
-    
-  //   if(val && !(password=="" || cpassword=="" || rollnumber==""|| email=="" || name1=="")){
-      
-  //     error.push("User already exits");
-  //     flag=1;
-  //     console.log(error);
-      
-      
-  //   }
-  //   if(rollnumber.length<8){
-  //     error.push("Enter a valid rollnumber");
-  //     flag=1;
-  //   }
-  //   var pt=/^[A-Za-z]+$/;
-  //   if((!(name1.match(pt)))&&(name1.length!=0)){
-  //     error.push("Enter a valid Name ");
-  //     flag=1;
-  //   }
-  //   if(cpassword!=password){
-  //     error.push("Passwords donot match");
-  //     flag=1;
-  //   }
-  //   if(password=="" || cpassword=="" || rollnumber==""|| email=="" || name1==""){
-  //     error.push("All details needs to be filed");
-  //     flag=1;
-  //   }
-  //   console.log(error);
-  //   if(flag==1){
-  //     var art=Array('ball','bat');
-  //     console.log(error.indexOf('hey man'));
-  //     console.log(error);
-  //     res.render("register.ejs",{array:error});
-  //   } 
-  //   else{
-    
-    User.register(new User({username: req.body.username,email:req.body.email,name: req.body.name}),req.body.password,function(err,user){
+  User.register(new User({username: req.body.username,email:req.body.email,name: req.body.name}),req.body.password,function(err,user){
         if(err){
             console.log(err);
-            res.render("login");
+            
+            res.render("login",{"error":"user already exists"});
         }
     passport.authenticate("local")(req,res,function(){
-        res.redirect("/");
+        res.render("login",{"success":"Successfully registered a new user"});
     })    
     })});
-    // })});
-
 app.get("/logout",(req,res)=>{
     req.logout();
     res.redirect('/');
@@ -116,8 +75,6 @@ function isLoggedIn(req,res,next) {
   }
   res.redirect("/login");
 }
-
-
 // Setting Up port on Localhost:3000
 app.listen(3000, function() {
     console.log("Server is running on port 3000.");
